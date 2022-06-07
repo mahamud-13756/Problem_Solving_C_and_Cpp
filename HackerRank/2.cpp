a53e7c37ebdf2c1521e1908f1a5891654296f925
Mahamud_Mredha <mahamud15-13756@diu.edu.bd>
2022-06-08 04:18:53 +0600

    2

--- HackerRank/2.cpp
+++ HackerRank/2.cpp
@@ -1,1 +1,19 @@
+#include<iostream>
+using namespace std;
 
+int main(){
+    int i;
+    long l;
+    char c;
+    float f;
+    double d;
+
+    scanf("%d %ld %c %f %lf",&i,&l,&c,&f,&d);
+
+    printf("%d\n%ld\n%c\n%f\n%lf\n",i,l,c,f,d);
+
+    return 0;
+
+
+
+}
