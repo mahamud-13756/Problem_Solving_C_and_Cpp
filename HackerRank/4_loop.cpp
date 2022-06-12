65dc1604f5c74c027f61fd798360f480d41cc07c
Mahamud_Mredha <mahamud15-13756@diu.edu.bd>
2022-06-13 05:54:32 +0600

    loop problem

---
+++ HackerRank/4_loop.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cin>>a;
+    cin>>b;
+
+    if(a<10)
+    {
+        for(int n=a; n<b+1 ; n++)
+        {
+            if(n==1)
+            {
+                printf("one\n");
+            }
+            else if(n==2)
+            {
+                printf("two\n");
+            }
+            else if(n==3)
+            {
+                printf("three\n");
+            }
+            else if(n==4)
+            {
+                printf("four\n");
+            }
+            else if(n==5)
+            {
+                printf("five\n");
+            }
+            else if(n==6)
+            {
+                printf("six\n");
+            }
+            else if(n==7)
+            {
+                printf("seven\n");
+            }
+            else if(n==8)
+            {
+                printf("eight\n");
+            }
+            else if(n==9)
+            {
+                printf("nine\n");
+            }
+
+
+            if(n>9){
+
+                if(n%2==0){
+                    printf("even\n");
+                }
+                else{
+                    printf("odd\n");
+                }
+            }
+        }
+    }
+
+    return 0;
+}
