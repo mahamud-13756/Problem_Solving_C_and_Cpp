4b1977a7584ca0dc15029d6548b6f350e3ffc238
Mahamud_Mredha <mahamud15-13756@diu.edu.bd>
2022-06-20 03:13:13 +0600

    CodeForces/50A_Domino_piling.cpp

---
+++ CodeForces/50A_Domino_piling.cpp
@@ -0,0 +1,14 @@
+// https://codeforces.com/problemset/problem/50/A
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int M,N,a;
+
+    cin>>M>>N;
+
+    a=(M*N)/2;
+    cout<<a<<endl;
+}
