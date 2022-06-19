3a2b2a499ab3061e33d8c613275a33ea631258f2
Mahamud_Mredha <mahamud15-13756@diu.edu.bd>
2022-06-20 02:46:30 +0600

    CodeForces/158A_Next_Round.cpp

--- CodeForces/158A_Next_Round.cpp
+++ CodeForces/158A_Next_Round.cpp
@@ -1,1 +1,32 @@
+//https://codeforces.com/problemset/problem/158/A
 
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int a[n],k,cnt=0,i;
+    cin>>k;
+
+    for(i=0; i<n; i++)
+    {
+        cin>>a[i];
+
+    }
+
+    for(i=0; i<n; i++)
+    {
+        if(a[i]>=a[k-1] && a[i]>0)
+        {
+            cnt++;
+        }
+    }
+
+    cout<<cnt<<endl;
+
+
+
+    return 0;
+}
