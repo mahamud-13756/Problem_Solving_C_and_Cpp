e25131efc65d21d37f07144fbb588aab0a66a0f2
Mahamud_Mredha <mahamud15-13756@diu.edu.bd>
2022-07-07 14:41:32 +0600

    29.11.1_pointers.cpp

---
+++ 29.11.1_pointers.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a=10;
+
+    int *aptr;
+
+    aptr=&a;
+
+    cout<<a<<endl;
+    cout<<aptr<<endl;
+    cout<<*aptr<<endl;
+
+
+    return 0;
+}
