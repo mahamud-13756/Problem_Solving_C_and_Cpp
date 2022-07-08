68eefde30b2ad0af3870e8c702758363defa5e1c
Mahamud_Mredha <mahamud15-13756@diu.edu.bd>
2022-07-08 14:56:12 +0600

    HackerRank/9_Pointer.cpp

---
+++ HackerRank/9_Pointer.cpp
@@ -0,0 +1,34 @@
+//
+
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+using namespace std;
+
+void update(int *a,int *b)
+{
+
+    int x,y;
+    x = *a + *b;
+    y = *a - *b;
+    *a = x;
+    *b =abs(y);
+}
+
+int main()
+{
+    int a,b;
+
+    int *aptr=&a,*bptr=&b;
+
+    cin>>a>>b;
+    cout<<a<<endl;
+    cout<<b<<endl;
+
+    update(aptr, bptr);
+
+    cout<<a<<endl;
+    cout<<b<<endl;
+
+    return 0;
+}
