fa9532274cd315f510e925caa89276dfe0695e7b
Mahamud_Mredha <mahamud15-13756@diu.edu.bd>
2022-07-09 10:21:10 +0600

    18.8.3_Array_min_max.cpp

--- 18.8.3_Array_min_max.cpp
+++ 18.8.3_Array_min_max.cpp
@@ -1,1 +1,36 @@
+#include<iostream>
+#include<climits>
+using namespace std;
 
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    int a[n],Max=INT_MIN,Min=INT_MAX;//<climits> max and min value of c++
+
+    for(int i=0; i<n; i++)
+    {
+        cin>>a[i];
+    }
+
+    for(int i=0; i<n; i++)
+    {
+
+        if(Max<a[i])
+        {
+            Max=a[i];
+        }
+
+        if(Min>a[i])
+        {
+            Min=a[i];
+        }
+    }
+
+    cout<<"Max value: "<<Max<<endl;
+    cout<<"Min value: "<<Min<<endl;
+
+    return 0;
+}
