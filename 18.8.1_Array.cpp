cb237d843ceb55946ebaac892e419e6c1f8c0804
Mahamud_Mredha <mahamud15-13756@diu.edu.bd>
2022-07-09 09:38:29 +0600

    18.8.1_Array.cpp

---
+++ 18.8.1_Array.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+    int arr[4];
+
+    arr[0]=5;
+    arr[1]=10;
+    arr[2]=15;
+    arr[3]=20;
+
+    cout<<arr[2]<<endl;
+
+    return 0;
+}
