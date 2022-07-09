dc42682a24be04c2f91d98007c8cddb346b983c8
Mahamud_Mredha <mahamud15-13756@diu.edu.bd>
2022-07-09 09:52:58 +0600

    all

--- 18.8.1_Array.cpp
+++ 18.8.1_Array.cpp
@@ -4,12 +4,14 @@ using namespace std;
 
 int main()
 {
-    int arr[4];
+////    int ARR[4];
+////
+////    ARR[0]=5;
+////    ARR[1]=10;
+////    ARR[2]=15;
+////    ARR[3]=20;
 
-    arr[0]=5;
-    arr[1]=10;
-    arr[2]=15;
-    arr[3]=20;
+    int arr[4]={5,10,15,20};
 
     cout<<arr[2]<<endl;
 
