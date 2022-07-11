7eb22a5cb26ff35cad0a0758748ee8239bb835d0
Mahamud_Mredha <mahamud15-13756@diu.edu.bd>
2022-07-11 16:51:12 +0600

    HackerRank/10_Diagonal_Difference.cpp

---
+++ HackerRank/10_Diagonal_Difference.cpp
@@ -0,0 +1,45 @@
+// https://www.hackerrank.com/challenges/diagonal-difference/problem?isFullScreen=true
+
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int main(){
+
+    int n;
+    cin>>n;
+    int matrix[n][n],SumOfDiagonal1=0,SumOfDiagonal2=0,DiagonalDeference;
+
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            cin>>matrix[i][j];
+        }
+    }
+
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            if(i==j){
+               SumOfDiagonal1+=matrix[i][j];
+            }
+        }
+    }
+
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            if(i==n-j-1){
+                SumOfDiagonal2+=matrix[i][j];
+            }
+        }
+    }
+
+
+    DiagonalDeference=abs(SumOfDiagonal1-SumOfDiagonal2);
+
+    cout<<DiagonalDeference<<endl;
+
+
+    return 0;
+
+
+
+}
