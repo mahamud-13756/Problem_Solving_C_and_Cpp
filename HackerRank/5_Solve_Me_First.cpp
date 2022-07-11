34731b598ec351a2c33d72e3d6986deb1d39a1a8
Mahamud_Mredha <mahamud15-13756@diu.edu.bd>
2022-07-11 13:32:50 +0600

    HackerRank/5_Solve_Me_First.cpp

---
+++ HackerRank/5_Solve_Me_First.cpp
@@ -0,0 +1,23 @@
+//https://www.hackerrank.com/challenges/solve-me-first/problem?isFullScreen=true
+
+#include<iostream>
+using namespace std;
+
+
+int solveMeFirst(int a, int b) {
+
+    int sumf=a+b;
+
+    return sumf;
+
+
+}
+
+int main() {
+  int num1, num2;
+  int sum;
+  cin>>num1>>num2;
+  sum = solveMeFirst(num1,num2);
+  cout<<sum;
+  return 0;
+}
