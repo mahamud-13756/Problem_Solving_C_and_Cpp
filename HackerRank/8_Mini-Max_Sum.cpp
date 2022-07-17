b53d1c44871638fee273eaadf385fc3693d8ded5
Mahamud_Mredha <mahamud15-13756@diu.edu.bd>
2022-07-17 22:55:55 +0600

    HackerRank/8_Mini-Max_Sum.cpp

--- HackerRank/8_Mini-Max_Sum.cpp
+++ HackerRank/8_Mini-Max_Sum.cpp
@@ -3,7 +3,28 @@ using namespace std;
 
 int main()
 {
-    int Array[5];
+    int Array[5],total_sum=0,Esum=0;
+
+    for(int i=0; i<5; i++){
+        cin>>Array[i];
+
+    }
+
+    for(int i=0; i<5; i++){
+        total_sum=total_sum+Array[i];
+
+
+    }
+    cout<<"Total sum: "<<total_sum<<endl;
+
+    for(int i=0; i<5; i++){
+        if(Array[i]==Array[i]){
+            Esum=total_sum-Array[i];
+            cout<<"Esum "<<i<<": "<<Esum<<endl;
+        }
+
+    }
+
 
 
     return 0;
