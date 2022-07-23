92651a4f3541d4a6f146ebf15afa809ba7c551d5
Mahamud_Mredha <mahamud15-13756@diu.edu.bd>
2022-07-23 12:29:07 +0600

    final code

--- HackerRank/8_Mini-Max_Sum.cpp
+++ HackerRank/8_Mini-Max_Sum.cpp
@@ -1,33 +1,29 @@
-// https://www.hackerrank.com/challenges/mini-max-sum/problem?utm_campaign=challenge-recommendation&utm_medium=email&utm_source=24-hour-campaign
-
-#include<iostream>
+#include <algorithm>
+#include <string.h>
+#include <vector>
+#include <cstdio>
+#include <climits>
+#include <iostream>
 using namespace std;
 
-int main()
+
+int main ()
 {
-    long long Array[5],total_sum=0,Esum=0,MIN=LONG_MIN,MAX=LONG_MAX;
 
+    long long  N=5, arr[N],totalSum=0,Esum=0,Min = LLONG_MAX, Max = LLONG_MIN;
 
-    for(int i=0; i<5; i++){
-        cin>>Array[i];
-        total_sum=total_sum+Array[i];
-
+    for (int i=1; i<=N; i++)
+    {
+        cin>>arr[i];
+        totalSum += arr[i];
     }
 
-
-
-
-
-    for(int i=0; i<5; i++){
-        Esum=total_sum-Array[i];
-        MIN=min(MIN,Esum);
-        MAX=max(MAX,Esum);
-
+    for (int i=1; i<=N; i++)
+    {
+        Esum = totalSum - arr[i];
+        Min = min(Min, Esum);
+        Max = max(Max, Esum);
     }
 
-    cout<<MIN<<" "<<MAX<<endl;
-
-
-
-    return 0;
+    cout<<Min<<" "<<Max<<endl;
 }
