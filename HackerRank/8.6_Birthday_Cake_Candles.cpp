6af915524fa0958c62edd4e1370d060f4626519c
Mahamud_Mredha <mahamud15-13756@diu.edu.bd>
2022-07-27 22:41:26 +0600

    HackerRank/8.6_Birthday_Cake_Candles.cpp

---
+++ HackerRank/8.6_Birthday_Cake_Candles.cpp
@@ -0,0 +1,31 @@
+//https://www.hackerrank.com/challenges/birthday-cake-candles/problem?isFullScreen=true
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    long arr[n],tallest=0,Count=0;
+
+    for(long i=0; i<n; i++){
+        cin>>arr[i];
+        if(tallest<arr[i]){
+            tallest=arr[i];
+        }
+
+    }
+
+    for(long i=0; i<n; i++){
+        if(tallest==arr[i]){
+            Count++;
+        }
+
+    }
+    cout<<tallest<<Count<<endl;
+
+
+    return 0;
+}
