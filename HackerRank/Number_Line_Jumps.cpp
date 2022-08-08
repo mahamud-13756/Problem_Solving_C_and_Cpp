93be9b79ad72e099f294b9e384891b0c2d2d16aa
Mahamud_Mredha <mahamud15-13756@diu.edu.bd>
2022-08-08 23:38:20 +0600

    HackerRank/Number_Line_Jumps.cpp

--- HackerRank/Number_Line_Jumps.cpp
+++ HackerRank/Number_Line_Jumps.cpp
@@ -1,38 +1,48 @@
-//https://www.hackerrank.com/challenges/kangaroo/problem?isFullScreen=true
-
 #include<bits/stdc++.h>
 using namespace std;
 
 
 int main()
 {
-    int x1,v1,value1=x1,jump1=0;
-    int x2,v2,value2=x2,jump2=0,M=INT_MAX,chk=0;
+    int x1,v1,value1=x1;
+    int x2,v2,value2=x2;
 
     cin>>x1>>v1>>x2>>v2;
 
     if(x1>x2 && v1>v2 || x2>x1 && v2>v1){
         cout<<"NO"<<endl;
     }
-    for(int i=0; i<M; i++){
-            value1+=value1;
-            jump1++;
-
-            value2+=value2;
-            jump2++;
-
-            if(value1==value2 && jump1==jump2){
-                chk=1;
+    else if(x1<x2 && v1 > v2) {
+        while(x1<=x2) {
+            if(x1==x2) {
+                cout<<"YES"<<endl;
                 break;
             }
+            else {
+                x1 = x1 + v1;
+                x2 = x2 + v2;
+            }
         }
-
-    if(chk==1){
-
-        cout<<"YES"<<endl;
-
-
+        if ( x1!= x2 ) {
+            cout<<"NO"<<endl;
+        }
     }
+    else if(x1>x2 && v1 < v2) {
+        while(x1>=x2) {
+            if(x1==x2) {
+                cout<<"YES"<<endl;
+                break;
+            }
+            else {
+                x1 = x1 + v1;
+                x2 = x2 + v2;
+            }
+        }
+        if ( x1!= x2 ) {
+            cout<<"NO"<<endl;
+        }
+    }
+
     else{
         cout<<"NO"<<endl;
     }
