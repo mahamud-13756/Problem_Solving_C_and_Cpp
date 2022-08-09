b50a158fa404dce16d258498009b5ba2b3a4c323
Mahamud_Mredha <mahamud15-13756@diu.edu.bd>
2022-08-09 13:27:36 +0600

    CodeForces/339A_Helpful_Maths.cpp

--- CodeForces/339A_Helpful_Maths.cpp
+++ CodeForces/339A_Helpful_Maths.cpp
@@ -3,11 +3,33 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+
 int main()
 {
+
+    string s,ns;
+    cin>>s;
+
+    int l=s.length();
+    sort(s.begin(), s.end()); // sort function included in <algorithm>
+
+
+    int a=ceil(l/2);
+
+
+    for(int i=a; i<l; i++){
+
+        cout<<s[i];
+
+        if(i<l-1){
+            cout<<"+";
+        }
+    }
 
 
 
 
     return 0;
 }
+
+
