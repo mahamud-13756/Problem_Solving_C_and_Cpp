024e37878b516fa97dd9ab19867abb6c1fc132ee
Mahamud_Mredha <mahamud15-13756@diu.edu.bd>
2022-08-13 23:31:45 +0600

    Character assign in any string

---
+++ CodeForces/281A_Word_Capitalization.cpp
@@ -0,0 +1,17 @@
+//https://codeforces.com/problemset/problem/281/A
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s;
+
+    cin>>s;
+
+    cin>>s[0];
+
+    cout<<s;
+
+    return 0;
+}
