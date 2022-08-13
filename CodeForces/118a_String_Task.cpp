0f6ce2b8c49a9fac418feb1b8a6a4b8bc7cf21f1
Mahamud_Mredha <mahamud15-13756@diu.edu.bd>
2022-08-13 19:13:11 +0600

    solved

--- CodeForces/118a_String_Task.cpp
+++ CodeForces/118a_String_Task.cpp
@@ -5,11 +5,29 @@ using namespace std;
 
 int main()
 {
+    int l;
     string s;
     cin>>s;
 
+
     transform(s.begin(), s.end(), s.begin(), ::tolower);
     cout << s << endl;
+    for ( l = 0; s[l]; l++);
+
+    cout << l << endl;
+
+    for(int i=0; i<l; i++){
+
+       if(s[i]=='a' || s[i]=='i' || s[i]=='o' || s[i]=='e' || s[i]=='u' || s[i]=='y'){
+
+            continue;
+
+       }
+       else{
+        cout<<"."<<s[i];
+       }
+
+    }
 
 
 
