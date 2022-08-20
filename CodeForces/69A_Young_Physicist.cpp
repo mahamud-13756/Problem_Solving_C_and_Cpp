d8807b348a9039834c3255d84d004634592ab431
Mahamud_Mredha <mahamud15-13756@diu.edu.bd>
2022-08-20 23:51:43 +0600

    CodeForces/69A_Young_Physicist.cpp

--- CodeForces/69A_Young_Physicist.cpp
+++ CodeForces/69A_Young_Physicist.cpp
@@ -6,28 +6,38 @@ using namespace std;
 
 int main()
 {
-    int Size,sum=0;
-    cin>>Size;
+    int Size1,Size2=3, c1=0, c2=0, c3=0;
+    cin>>Size1;
 
-    int a[Size][Size];
+    int a[Size1][Size2];
 
-    for(int i=0; i<Size; i++){
-        for(int j=0; j<Size; j++){
+    for(int i=0; i<Size1; i++){
+        for(int j=0; j<Size2; j++){
             cin>>a[i][j];
         }
     }
 
-    for(int i=0; i<Size; i++){
-        for(int j=0; j<Size; j++){
+    for(int i=0; i<Size1; i++){
+        for(int j=0; j<Size2; j++){
             //cout<<a[i][j]<<" ";
-            sum+=a[i][j];
+            //sum+=a[i][j];
+            if(j==0){
+                c1+= a[i][j];
+            }
+            if(j==1){
+                c2+= a[i][j];
+            }
+            if(j==2){
+                c3+= a[i][j];
+            }
+
         }
         //cout<<endl;
     }
 
     //cout<<sum;
 
-    if(sum==0){
+    if(c1==0 && c2==0 && c3==0){
         cout<<"YES"<<endl;
     }
     else{
