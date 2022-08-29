f80c6682b8e69e91278e2be91b0a08ca3556c02d
Mahamud_Mredha <mahamud15-13756@diu.edu.bd>
2022-08-29 22:07:15 +0600

    solved

--- CodeForces/Task2.cpp
+++ CodeForces/Task2.cpp
@@ -1,10 +1,57 @@
+/*
+Name: Md. Mahamud Mredha
+Pattern : MAH*AMUD*
+*/
+
 #include<bits/stdc++.h>
 using namespace std;
 
 int main()
 {
-    string s;
-    cin>>s;
+    string c, s="MAHAMUD";
+
+    cin>>c;
+    int len=c.size(), as=0,j=0,a=0;
+    //cout<<len<<endl;
+
+    for(int i=0; i<len; i++){
+        a=0;
+        as=(int) c[i];
+       // cout<<as<<endl;
+
+        if(as%2!=0 && c[i]==s[j]){
+            a=1;
+            j++;
+//            cout<<"M J"<<endl;
+//            cout<<endl;
+
+        }
+        if(as%2==0 && c[i]==s[j]){
+            a=1;
+
+//            cout<<"M"<<endl;
+//            cout<<endl;
+            if(c[i]!=c[i+1]){
+                j++;
+            }
+        }
+//        else{
+//            //cout<<"Does not Match"<<endl;
+//            a=0;
+//        }
+
+    }
+
+    if(a==1){
+        cout<<"Match"<<endl;
+    }
+
+    if(a==0){
+        cout<<"Does not Match"<<endl;
+    }
+
+
+
 
 
 
