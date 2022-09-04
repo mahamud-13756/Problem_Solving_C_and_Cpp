e8a2221b657472195d155e3389e2cfa9942ca0f5
Mahamud_Mredha <mahamud15-13756@diu.edu.bd>
2022-09-04 23:35:36 +0600

    Letter_Count_loop.cpp

--- Letter_Count_loop.cpp
+++ Letter_Count_loop.cpp
@@ -4,11 +4,16 @@ using namespace std;
 int main()
 {
     string s;
+    cin>>s;
 
     int hsh[26];
 
     for(int i=0; i<26; i++){
+        hsh[i]=0;
+    }
 
+    for(int i=0; i<26; i++){
+        hsh[s[i]-'A']++;
     }
 
 
