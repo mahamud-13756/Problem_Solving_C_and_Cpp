c60238aba2279964729d65bc795a112b45272b1c
Mahamud_Mredha <mahamud15-13756@diu.edu.bd>
2022-09-11 15:17:02 +0600

    aschi value

--- SamsungTraining/Untitled3.cpp
+++ SamsungTraining/Untitled3.cpp
@@ -6,7 +6,12 @@ int main(){
     for(i=28; i>0; i-=4)
         if((n>>i)&15)
             break;
-    for(; i>=0; i-=4)
-        cout<<((n>>i)&15);
+    for(; i>=0; i-=4){
+        int digit=(n>>i)&15;
+        if(digit<10)
+            printf("%d", digit);
+        else
+            printf("%d", digit+55);
+    }
     return 0;
 }
