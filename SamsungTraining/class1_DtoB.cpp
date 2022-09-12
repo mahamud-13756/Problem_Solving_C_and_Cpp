d9d7e5357c29aa827415bc46de42a149b3fccfcb
Mahamud_Mredha <mahamud15-13756@diu.edu.bd>
2022-09-12 23:44:56 +0600

    Decimal to Binary

--- SamsungTraining/class1_DtoB.cpp
+++ SamsungTraining/class1_DtoB.cpp
@@ -1,18 +1,16 @@
-// Binary to decimal
+// Decimal to binary
 
 #include<bits/stdc++.h>
 using namespace std;
 
 int main()
 {
-    int n, count=0;
-   cin>>n;
-   // n er moddhe joto 1 thakbe totobar loop cholbe
-   while(n){
-    n=n&(n-1);
-    count++;
-   }
-   cout<<count;
+    int n;
+    cin>>n;
+
+    for(int i=31; i>=0; i--){
+        cout<<((n>>i)&1);
+    }
 
 
     return 0;
