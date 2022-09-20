059619a8f519b09208057a23ba139d9687043add
Mahamud_Mredha <mahamud15-13756@diu.edu.bd>
2022-09-20 23:44:31 +0600

    CodeForces/131A_cAPS_lOCK.cpp

---
+++ CodeForces/131A_cAPS_lOCK.cpp
@@ -0,0 +1,50 @@
+// https://codeforces.com/problemset/problem/131/A
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int c = 0,d = 0;
+    string s;
+    cin >> s;
+    for(int i = 1; i < s.size(); i++)
+    {
+        if(islower(s[i]))
+        {
+            c++;
+        }
+    }
+    for(int i = 0; i < s.size(); i++)
+    {
+        if(isupper(s[i]))
+        {
+            d++;
+        }
+    }
+    if(islower(s[0]) && c==0)
+    {
+        s[0] = toupper(s[0]);
+        for(int i = 1; i < s.size(); i++)
+        {
+            s[i] = tolower(s[i]);
+        }
+        cout << s;
+        return 0;
+    }
+    if(d==s.size())
+    {
+        for(int i=0; i<s.size(); i++)
+        {
+            s[i]=tolower(s[i]);
+        }
+        cout<<s;
+    }
+    else
+    {
+        cout<<s;
+    }
+
+
+    return 0;
+}
