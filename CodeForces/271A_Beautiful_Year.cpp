66c50e0479b0c5a1131972d36f49e14f9c22416b
Mahamud_Mredha <mahamud15-13756@diu.edu.bd>
2022-09-25 23:57:42 +0600

    CodeForces/271A_Beautiful_Year.exe

--- CodeForces/271A_Beautiful_Year.cpp
+++ CodeForces/271A_Beautiful_Year.cpp
@@ -5,37 +5,92 @@ using namespace std;
 
 int main()
 {
+    int y,a,b,c,d;
+    cin>>y;
 
-    int n,y[],f=1,i,j;
-    cin>>n;
+    while(1){
+        y++;
 
-    for(int k=0; k<4; k++){
-        y[k]=n[k];
+        a=y/1000;
+        b=(y/100)%10;
+        c=(y/10)%10;
+        d = y % 10;
+
+        if (a != b && a != c && a != d && b != c && b != d && c != d)
+        {
+            break;
+        }
     }
-
-    y++;
-    for(i=0; i<4; i++){
-        for(j=1; j<4; j++){
-            if(y[i]==y[j+i]){
-                f=0;
-                y++;
-                break;
-            }
-            else{
-
-                continue;
-            }
-
-    }
-
-    }
-    if(f==0){
-
-    }
-    else{
-        cout<<y;
-    }
-
+    cout << y << endl;
 
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//#include<bits/stdc++.h>
+//using namespace std;
+//
+//int main()
+//{
+//
+//    int n,y[],f=1,i,j;
+//    cin>>n;
+//
+//    for(int k=0; k<4; k++){
+//        y[k]=n[k];
+//    }
+//
+//    y++;
+//    for(i=0; i<4; i++){
+//        for(j=1; j<4; j++){
+//            if(y[i]==y[j+i]){
+//                f=0;
+//                y++;
+//                break;
+//            }
+//            else{
+//
+//                continue;
+//            }
+//
+//    }
+//
+//    }
+//    if(f==0){
+//
+//    }
+//    else{
+//        cout<<y;
+//    }
+//
+//
+//    return 0;
+//}
