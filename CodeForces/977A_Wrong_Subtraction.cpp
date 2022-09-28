69fd0740001126f5760882bd5a38d1d2dd81e927
Mahamud_Mredha <mahamud15-13756@diu.edu.bd>
2022-09-28 06:58:29 +0600

    CodeForces/977A_Wrong_Subtraction.cpp

---
+++ CodeForces/977A_Wrong_Subtraction.cpp
@@ -0,0 +1,25 @@
+//https://codeforces.com/problemset/problem/977/A
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n,k,a;
+    cin>>n>>k;
+
+    for(int i=0; i<k; i++){
+        if(n%10==0){
+            n=n/10;
+        }
+        else{
+            n=n-1;
+
+        }
+        }
+
+
+    cout<<n;
+
+    return 0;
+}
