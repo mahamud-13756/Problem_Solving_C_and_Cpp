b13ebcd0d51de0b45e727b73741931a31fb13120
Mahamud_Mredha <mahamud15-13756@diu.edu.bd>
2022-10-13 23:58:38 +0600

    CodeForces/110A_Nearly_Lucky_Number.cpp

---
+++ CodeForces/110A_Nearly_Lucky_Number.cpp
@@ -0,0 +1,29 @@
+//https://codeforces.com/problemset/problem/110/A
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long long int n;
+    int l=0;
+    cin>>n;
+
+    //for(int i=0; i<n; i++){
+        if(n%10==4 || n%10==7){
+
+            n=n/10;
+            cout<<n;
+        }
+        if(n==4 || n==7){
+            cout<<
+        }
+
+   // }
+
+
+
+
+
+    return 0;
+}
