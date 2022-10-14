e4c41b2c674030fcbb80ef0f4ad5c4a4c4d8eee7
Mahamud_Mredha <mahamud15-13756@diu.edu.bd>
2022-10-15 00:18:34 +0600

    https://www.geeksforgeeks.org/write-a-program-to-reverse-an-array-or-string/    Sheet/Reverse_the_array.cpp

--- Sheet/Reverse_the_array.cpp
+++ Sheet/Reverse_the_array.cpp
@@ -3,6 +3,16 @@ using namespace std;
 
 int main()
 {
+    int n;
+    cin>>n;
+    int a[n];
+
+    for(int i=0; i<n; i++){
+        cin>>a[i];
+    }
+    for(int i=0; i<n; i++){
+        cout<<a[n-i-1]<<" ";
+    }
 
     return 0;
 }
