7f4436895b9c3224a5053d830df8b5d27d3a7742
Mahamud_Mredha <mahamud15-13756@diu.edu.bd>
2022-10-28 17:44:32 +0600

    CodeForces/706B_Interesting_drink.cpp

--- CodeForces/706B_Interesting_drink.cpp
+++ CodeForces/706B_Interesting_drink.cpp
@@ -3,22 +3,64 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+
+int main()
+{
+    int m,n,i,j,k,ans;
+    cin>>n;
+    int a[n],p;
+    int cnt=0;
+    for(i=0; i<n; i++)
+    {
+        cin>>a[i];
+    }
+    cin>>m;
+    sort(a,a+n);
+    while(m--)
+    {
+        cin>>k;
+        ans=upper_bound(a,a+n, k)-a;
+
+        cout<<ans<<endl;
+    }
+return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+#include<bits/stdc++.h>
+using namespace std;
+
 int main()
 {
     int n;
     cin>>n;
-    int arr[n];
+     int arr[n];
     for(int i=0; i<n; i++){
         cin>>arr[i];
     }
 
-    int q;
+     int q;
     cin>>q;
-    int mrr[q];
+     int mrr[q];
     for(int i=0; i<q; i++){
         cin>>mrr[i];
     }
-    int ans;
+     int ans;
     for(int i=0; i<q; i++){
         ans=0;
         for(int j=0; j<n; j++){
@@ -33,3 +75,4 @@ int main()
 
     return 0;
 }
+*/
