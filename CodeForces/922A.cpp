f6017762752d7283576c7dfcbd77d41d0588efa9
Mahamud_Mredha <mahamud15-13756@diu.edu.bd>
2024-01-31 15:48:24 +0600

    CodeForces/922A.cpp

---
+++ CodeForces/922A.cpp
@@ -0,0 +1,34 @@
+//
+//
+#include<bits/stdc++.h>
+using namespace std;
+
+/////////////////////////////////////////////////////////////////
+#define ll                  long long int
+#define ull                 unsigned long long
+
+
+/////////////////////////////////////////////////////////////////
+
+
+
+int main()
+{
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        ll n,m,st;
+        cin>>n>>m;
+
+        st=m/2;
+        cout<<st*n<<"\n";
+
+
+    }
+
+
+    return 0;
+}
+
+
