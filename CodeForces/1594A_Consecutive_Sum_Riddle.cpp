bc40c452120228eb70b8277efe696c18f092adaa
Mahamud_Mredha <mahamud15-13756@diu.edu.bd>
2024-04-25 02:04:53 +0600

    1594A_Consecutive_Sum_Riddle.cpp

---
+++ CodeForces/1594A_Consecutive_Sum_Riddle.cpp
@@ -0,0 +1,23 @@
+//========“In the name of ALLAH, The Most Gracious and The Most Merciful“===============================|
+    // problem link : https://codeforces.com/contest/1594/problem/A
+    // Name         : .cpp
+    // Author       : Md. Mahamud Mredha
+    // Version      :
+    // Copyright    : use it under your responsibility
+    // Description  : Rating 900, Ansi-style
+    // status       : accepted
+//=======================================================================================================|
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int tc; cin>>tc;
+    while(tc--)
+    {
+        long long n; cin>>n;
+
+        cout<<-(n-1)<<" "<<n<<endl;
+    }
+    return 0;
+}
